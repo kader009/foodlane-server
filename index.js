c206d8b298b6f145d26509fd3621ae7bcf3436ad
kader009 <kadermollarasel000@gmail.com>
2024-09-01 11:13:47 +0600

    foodlane server create

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Restaurant server is on');
+});
+
+app.listen(port, (req, res) => {
+  console.log(`restaurant is on port ${port}`);
+});
