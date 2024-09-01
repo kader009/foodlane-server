be8e43698cd7295e9e180ede6d5f85d253e655d4
kader009 <kadermollarasel000@gmail.com>
2024-09-01 11:30:55 +0600

    mongodb add

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv'
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 app.use(cors());
+dotenv.config()
+
+const uri = process.env.DB_URL;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Restaurant server is on');
