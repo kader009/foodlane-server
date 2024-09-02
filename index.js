8bcd449e7820b61b499d96ba16f36f1375c3cb04
kader009 <kadermollarasel000@gmail.com>
2024-09-02 14:45:22 +0600

    email query done

--- index.js
+++ index.js
@@ -28,11 +28,26 @@ async function run() {
     const FoodCollection = database.collection('foodData');
     const userCollection = database.collection('User');
 
-    app.get('/foodData', async (req, res) => {
-      const result = await FoodCollection.find().toArray();
+    app.get('/foodData', async (req, res) => { 
+      console.log(req.query.email);
+      let query = {};
+
+      if (req.query?.email) {
+        query = { 'addBy.email': req.query.email };
+      }
+
+      const result = await FoodCollection.find(query).toArray();
 
       res.send(result);
     });
+
+
+    // post fooddata
+    app.post('/foodData', async(req, res) =>{
+      const food = req.body;
+      const result = await FoodCollection.insertOne(food)
+      res.send(result)
+    })
 
     app.get('/foodDataCount', async (req, res) => {
       const count = await FoodCollection.estimatedDocumentCount();
@@ -42,8 +57,32 @@ async function run() {
     // user post request
     app.post('/user', async (req, res) => {
       const user = req.body;
+      const { email } = user;
+      const exists = await userCollection.findOne({ email });
+
+      if (exists) {
+        return res.send('user already exists');
+      }
+
       const result = await userCollection.insertOne(user);
       res.send(result);
+    });
+
+    // login user
+    app.post('/login', async (req, res) => {
+      const { email, password } = req.body;
+
+      const user = await userCollection.findOne({ email });
+
+      if (!user) {
+        res.status(404).send({ error: 'user not found' });
+      }
+
+      if (user.password !== password) {
+        res.status(401).send({ error: 'invalid password' });
+      }
+
+      res.send(user);
     });
 
     console.log(
