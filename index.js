93fe1a3c65d866255136b218bdf255b5cad7bd7f
kader009 <kadermollarasel000@gmail.com>
2024-09-26 15:30:33 +0600

    origin add

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: '*',
+    origin: ['https://foodlane-server-api.onrender.com'],
     methods: ['GET','POST','PATCH','DELETE'],
     credentials: true,
   })
